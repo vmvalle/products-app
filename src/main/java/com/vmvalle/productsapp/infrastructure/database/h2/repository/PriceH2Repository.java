49151f53694d167c5f48f1df9063aff519049e34
vmvalle <victormvs022@gmail.com>
2022-03-07 13:13:48 +0100

    Added H2 repository in infrastructure layer

---
+++ src/main/java/com/vmvalle/productsapp/infrastructure/database/h2/repository/PriceH2Repository.java
@@ -0,0 +1,25 @@
+package com.vmvalle.productsapp.infrastructure.database.h2.repository;
+
+import com.vmvalle.productsapp.domain.entity.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface PriceH2Repository extends JpaRepository<Price, Integer> {
+
+    /**
+     * Find price of product by product identifier and brand identifier in selected date.
+     *
+     * @param productId Product identifier.
+     * @param brandId Brand identifier.
+     * @param selectedDate Selected date.
+     * @return Optional price.
+     */
+    @Query(value = "SELECT p FROM Price p WHERE p.productId =?1 AND p.brandId =?2 AND p.startDate <= ?3 AND p.endDate > ?3")
+    List<Price> findPricesByProductIdAndBrandIdOnSelectedDate(Long productId, Integer brandId, LocalDateTime selectedDate);
+
+}
---
+++ src/test/java/com/vmvalle/productsapp/unit/infrastructure/database/PriceRepositoryTests.java
@@ -0,0 +1,45 @@
+package com.vmvalle.productsapp.unit.infrastructure.database;
+
+import com.vmvalle.productsapp.domain.entity.Price;
+import com.vmvalle.productsapp.domain.repository.PriceRepository;
+import com.vmvalle.productsapp.infrastructure.database.h2.repository.PriceH2Repository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class PriceRepositoryTests {
+
+    @Autowired
+    private PriceRepository repository;
+
+    @MockBean
+    private PriceH2Repository priceH2Repository;
+
+    @Test
+    void shouldFindPricesByProductIdAndBrandIdOnSelectedDate() {
+        // given
+        var productId = 35455L;
+        var brandId = 1;
+        var selectedDate = LocalDateTime.of(2022, Month.JUNE, 14, 0, 0, 0);
+
+        var listPrices = List.of(mock(Price.class));
+        when(priceH2Repository.findPricesByProductIdAndBrandIdOnSelectedDate(productId, brandId, selectedDate)).thenReturn(listPrices);
+
+        // when
+        var prices = repository.findPricesByProductIdAndBrandIdOnSelectedDate(productId, brandId, selectedDate);
+
+        // then
+        assertFalse(prices.isEmpty());
+    }
+
+}
